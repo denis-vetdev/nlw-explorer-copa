7fa978929caac22a7210ef062dd2e4383a2948e9
Denis Oliveira <denis.vetdev@gmail.com>
2022-11-05 14:19:03 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function createGame(player1, hour, player2) {
+    return `
+        <li>    
+            <img src="./assets/icon-${player1}.svg" alt="Bandeira ${player1}"> <strong>${hour}</strong>
+            <img src="./assets/icon-${player2}.svg" alt="Bandeira ${player2}">
+        </li>
+    `
+}
+
+let delay =-0.4;
+
+function createCard(date, day, games) {
+    delay = delay + 0.4;
+    return `
+        <div class="card" style="animation-delay: ${delay}s">
+            <h2>${date} <span>${day}</span></h2>
+            <ul>
+              ${games}  
+            </ul>       
+        </div>          
+    `
+}
+
+document.querySelector('#cards').innerHTML =
+       createCard("24/11", "quinta", 
+       createGame('uruguay', '10:00', 'south-korea') + 
+       createGame('portugal', '13:00', 'ghana') + createGame('brazil', '16:00', 'serbia')) +
+       createCard("28/11", "segunda", 
+       createGame('south-korea', '10:00', 'ghana') + 
+       createGame('switzerland', '13:00', 'brazil') + createGame('portugal', '16:00', 'uruguay')) +
+       createCard("02/12", "sexta", 
+       createGame('ghana', '12:00', 'uruguay') + 
+       createGame('Serbia', '16:00', 'Switzerland') + createGame('cameroon', '16:00', 'brazil'))
